7fcc7c21afcb08c5ee35b47ae1b9602dea43e141
Patrick Divine <pddivine@Patricks-MacBook-Pro.local>
2017-04-07 18:40:37 -0700

    Updated logic to return native constructor functions as the type they are creating.

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 module.exports = typeOf;
 
 /**
- * Determine a variable's type.
+ * Determine an intuitive response when querying a variable's type.
  * 
  * @param {any} val
- * @returns {String} A variable's type.
+ * @returns {String} An intuitive determination of a variable's type.
  */
 function typeOf (val) {
   // Handle exceptions to typeof
   if (val === null) { return 'null'; }
   if (Array.isArray(val)) { return 'array'; }
+
+  // Handle constructors
+  switch (val) {
+    case Array    : return 'array';
+    case Boolean  : return 'boolean';
+    case Function : return 'function';
+    case Number   : return 'number';
+    case String   : return 'string';
+    case Symbol   : return 'symbol';
+  }
+  
   // typeof
   return typeof val;
 }
+
+console.log(typeOf(null))
